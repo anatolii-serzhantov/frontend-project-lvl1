320460b4979afc38de416825a895a24b49859cae
anatolii-serzhantov <serzhantov.anatoly@gmail.com>
2021-03-27 18:37:42 +0300

    fixing super-linter errors

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 import engine from '../src/index.js'
 
-const gameDescription = 'What is the result of the expression?';
+const gameDescription = 'What is the result of the expression?'
 
 const gameQuestion = () => {
-  const randomNumberOne = Math.floor(Math.random() * 100);
-  const randomNumberTwo = Math.floor(Math.random() * 100);
+  const randomNumberOne = Math.floor(Math.random() * 100)
+  const randomNumberTwo = Math.floor(Math.random() * 100)
   const operations = ['+', '-', '*']
-  const randomOperation = operations[Math.floor(Math.random() * operations.length)];
-  const question = `${randomNumberOne} ${randomOperation} ${randomNumberTwo}`;
-  return question;
+  const randomOperation = operations[Math.floor(Math.random() * operations.length)]
+  const question = `${randomNumberOne} ${randomOperation} ${randomNumberTwo}`
+  return question
 }
 
 const chekingAnswer = (expression) => {
-  function parse(str) {
+  function parse (str) {
     return Function(`'use strict'; return (${str})`)()
   }
 
   // console.log(parse(expression)) // 4.336886679999999
-  return parse(expression).toString(); // работающая старая строка
+  return parse(expression).toString() // работающая старая строка
 }
 
-engine(gameDescription, gameQuestion, chekingAnswer);
+engine(gameDescription, gameQuestion, chekingAnswer)
--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-import engine from '../src/index.js';
+import engine from '../src/index.js'
 
-const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const gameQuestion = () => {
-    const randomNumber = Math.floor(Math.random() * 100);
-    return randomNumber.toString();
+  const randomNumber = Math.floor(Math.random() * 100)
+  return randomNumber.toString()
 }
 
 const chekingAnswer = (expression) => {
-    const number = Number(expression)
-    let result = 'yes';
-    for (let i = 2; i < number; i += 1) {
-        if (number % i === 0) {
-            result = 'no';
-            break;
-        }
+  const number = Number(expression)
+  let result = 'yes'
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      result = 'no'
+      break
     }
-    return result;
+  }
+  return result
 }
 
-engine(gameDescription, gameQuestion, chekingAnswer);
+engine(gameDescription, gameQuestion, chekingAnswer)
