17cd320ac6eeeb5d9a7ce7841360a3c0a883c3b7
anatolii-serzhantov <serzhantov.anatoly@gmail.com>
2021-03-27 17:49:46 +0300

    brain-prime finished

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-import engine from '../src/index.js';
+import engine from '../src/index.js'
 
-const gameDescription = 'What is the result of the expression?';
+const gameDescription = 'What is the result of the expression?'
 const gameQuestion = () => {
-    const randomNumberOne = Math.floor(Math.random() * 100);
-    const randomNumberTwo = Math.floor(Math.random() * 100);
-    const operations = ['+', '-', '*'];
-    const randomOperation = operations[Math.floor(Math.random() * operations.length)];
-    const question = `${randomNumberOne} ${randomOperation} ${randomNumberTwo}`;
-    return question;
-};
+  const randomNumberOne = Math.floor(Math.random() * 100)
+  const randomNumberTwo = Math.floor(Math.random() * 100)
+  const operations = ['+', '-', '*']
+  const randomOperation = operations[Math.floor(Math.random() * operations.length)]
+  const question = `${randomNumberOne} ${randomOperation} ${randomNumberTwo}`
+  return question
+}
 
 const chekingAnswer = (expression) => {
-    return eval(expression).toString();
-};
+  return eval(expression).toString()
+}
 
-engine(gameDescription, gameQuestion, chekingAnswer);
+engine(gameDescription, gameQuestion, chekingAnswer)
--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
-import engine from '../src/index.js';
+import engine from '../src/index.js'
 
-const gameDescription = 'Find the greatest common divisor of given numbers.';
+const gameDescription = 'Find the greatest common divisor of given numbers.'
 const gameQuestion = () => {
-    const randomNumberOne = Math.floor(Math.random() * 100);
-    const randomNumberTwo = Math.floor(Math.random() * 100);
-    const question = `${randomNumberOne} ${randomNumberTwo}`;
-    return question;
-};
+  const randomNumberOne = Math.floor(Math.random() * 100)
+  const randomNumberTwo = Math.floor(Math.random() * 100)
+  const question = `${randomNumberOne} ${randomNumberTwo}`
+  return question
+}
 
 const gcd = (a, b) => {
-    if (!b) {
-        return a;
-    }
+  if (!b) {
+    return a
+  }
 
-    return gcd(b, a % b);
+  return gcd(b, a % b)
 }
 
 const chekingAnswer = (expression) => {
-    const numbers = expression.split(' ');
-    const firstNumber = parseInt(numbers[0]);
-    const secondNumber = parseInt(numbers[1]);
-    const result = gcd(firstNumber, secondNumber);
-    return result.toString();
-};
+  const numbers = expression.split(' ')
+  const firstNumber = parseInt(numbers[0])
+  const secondNumber = parseInt(numbers[1])
+  const result = gcd(firstNumber, secondNumber)
+  return result.toString()
+}
 
-engine(gameDescription, gameQuestion, chekingAnswer);
+engine(gameDescription, gameQuestion, chekingAnswer)
