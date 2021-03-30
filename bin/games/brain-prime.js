e9c8ab937b47adde29972c32ef4241f68d75eef1
anatolii-serzhantov <serzhantov.anatoly@gmail.com>
2021-03-30 17:37:57 +0300

    refactored before autotests

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import gameEngine from '../src/index.js'
-import { gameDescription, gameQuestion, gameAnswer } from '../src/games/prime.js'
+import gameEngine from '../src/index.js';
+import { gameDescription, gameQuestion, gameAnswer } from '../src/games/prime.js';
 
-gameEngine(gameDescription, gameQuestion, gameAnswer)
+gameEngine(gameDescription, gameQuestion, gameAnswer);
